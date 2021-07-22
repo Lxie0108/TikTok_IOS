2c6596bb2847a982766a936871ab03b62371f8b4
Ling <cathyxie@brandeis.edu>
2021-07-22 14:58:40 -0400

    Initial Commit

---
+++ TikTok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTok
+//
+//  Created by Ling Xie on 7/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
