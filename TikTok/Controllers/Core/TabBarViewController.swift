6438582d59857c7ad3e6556b21dad38303b710a4
Ling <cathyxie@brandeis.edu>
2021-07-27 11:58:11 -0400

    set up TabBar

--- TikTok/Controllers/Core/TabBarViewController.swift
+++ TikTok/Controllers/Core/TabBarViewController.swift
@@ -11,6 +11,8 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpControllers()
+        
 
         // Do any additional setup after loading the view.
     }
@@ -25,5 +27,31 @@ class TabBarViewController: UITabBarController {
         // Pass the selected object to the new view controller.
     }
     */
+    private func setUpControllers(){
+        let home = HomeViewController()
+        let explore = ExploreViewController()
+        let camera = CameraViewController()
+        let notifications = NotificationViewController()
+        let profile = ProfileViewController()
+        
+        home.title = "Home"
+        explore.title = "Explore"
+        notifications.title = "Notifications"
+        profile.title = "Profile"
+        
+        let nav1 = UINavigationController(rootViewController: home)
+        let nav2 = UINavigationController(rootViewController: explore)
+        let nav3 = UINavigationController(rootViewController: notifications)
+        let nav4 = UINavigationController(rootViewController: profile)
+        
+        nav1.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "house"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "safari"), tag: 2)
+        camera.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "camera"), tag: 3)
+        nav3.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "bell"), tag: 4)
+        nav4.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "person.crop.circle"), tag: 5)
+        
+        setViewControllers([nav1,nav2, camera, nav3, nav4], animated: false)
+        
+    }
 
 }
