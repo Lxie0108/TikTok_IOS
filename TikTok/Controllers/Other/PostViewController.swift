b900bceff57bbc1f638ba7a94acf72ef5b93413a
Ling <cathyxie@brandeis.edu>
2021-08-13 20:15:42 -0400

    added LikeButton,ShareButton, CommentButton, and doubleTap animation

--- TikTok/Controllers/Other/PostViewController.swift
+++ TikTok/Controllers/Other/PostViewController.swift
@@ -9,8 +9,42 @@ import UIKit
 
 class PostViewController: UIViewController {
     
-    let model: PostModel
+    var model: PostModel
     
+    private let likeButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "suit.heart"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.tintColor = .white
+        return button
+    }()
+    
+    private let commentButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "text.bubble"), for: .normal)
+        button.tintColor = .white
+        return button
+    }()
+    
+    private let shareButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
+        button.tintColor = .white
+        return button
+    }()
+    
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.text = "Check out this video! #foryou"
+        label.font = .systemFont(ofSize:24)
+        label.textColor = .white
+        return label
+    }()
+    
+    
+    // Init
     init(model: PostModel){
         self.model = model
         super.init(nibName: nil, bundle: nil)
@@ -24,10 +58,101 @@ class PostViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        let colors: [UIColor] = [.red, .green, .black, .blue, .systemPink]
+        let colors: [UIColor] = [.green, .black, .blue, .systemPink]
         view.backgroundColor = colors.randomElement()
+        
+        setUpButtons()
+        setUpDoubleTapToLike()
+        view.addSubview(captionLabel)
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let size: CGFloat = 40
+        let yStart: CGFloat = view.height - (size * 4) - 30 - view.safeAreaInsets.bottom -
+            (tabBarController?.tabBar.height ?? 0)
+        for (index, button) in [likeButton, commentButton, shareButton].enumerated(){
+            button.frame = CGRect(x: view.width-size-10, y: yStart + (CGFloat(index)*10) + (CGFloat(index) * size), width: size, height: size)
+        }
+        
+        captionLabel.sizeToFit()
+        let labelHeight = captionLabel.sizeThatFits(CGSize(width: view.width - size - 12, height:view.height))
+        captionLabel.frame = CGRect(x:5,
+                                    y: view.height - 10 - view.safeAreaInsets.bottom - labelHeight.height - (tabBarController?.tabBar.height ?? 0),
+                                    width: view.width - size - 12,
+                                    height:labelHeight.height)
+    }
+    
+    func setUpButtons(){
+        view.addSubview(likeButton)
+        view.addSubview(commentButton)
+        view.addSubview(shareButton)
+        
+        likeButton.addTarget(self, action: #selector(didTapLike), for: .touchUpInside)
+        commentButton.addTarget(self, action: #selector(didTapComment), for: .touchUpInside)
+        shareButton.addTarget(self, action: #selector(didTapShare), for: .touchUpInside)
+        
+    }
+    
+    @objc private func didTapLike(){
+        model.isLikedByCurrentUser = !model.isLikedByCurrentUser
+        likeButton.tintColor = model.isLikedByCurrentUser ? .systemRed : .white
+    }
+    
+    @objc private func didTapComment(){
+        //comment area
+    }
+    
+    @objc private func didTapShare(){
+        guard let url = URL(string:"https://www.tiktok.com") else{
+            return
+        }
+        let vc = UIActivityViewController(
+            activityItems: [url],
+            applicationActivities: []
+        )
+        present(vc,animated: true)
+    }
+    
+    func setUpDoubleTapToLike(){
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didDoubleTap(_:)))
+        tap.numberOfTapsRequired = 2
+        view.addGestureRecognizer(tap)
+        view.isUserInteractionEnabled = true
+    }
+    
+    @objc private func didDoubleTap(_ gesture: UITapGestureRecognizer){
+        if !model.isLikedByCurrentUser{
+            model.isLikedByCurrentUser = true
+        }
+        
+        let touchPoint = gesture.location(in: view)
+        let imageView = UIImageView(image: UIImage(systemName: "suit.heart"))
+        imageView.tintColor = .systemRed
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x:0, y:0, width:100, height: 100)
+        imageView.center = touchPoint
+        imageView.contentMode = .scaleAspectFit
+        imageView.alpha = 0
+        view.addSubview(imageView)
+        
+        UIView.animate(withDuration: 0.2){
+            imageView.alpha = 1
+        } completion: { done in
+            if done{
+                DispatchQueue.main.asyncAfter(deadline: .now()+0.2){
+                    UIView.animate(withDuration: 0.3){
+                        imageView.alpha = 0
+                    } completion: { done in
+                        if done{
+                            imageView.removeFromSuperview()
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
